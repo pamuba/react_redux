1f20fd487866a33c5f55cd499cf96657aea91f78
pamuba <pamuba@users.noreply.github.com>
2020-08-21 21:16:01 +0530

    9

--- src/App.js
+++ src/App.js
@@ -5,16 +5,19 @@ import CakeContainer from './components/CakeContainer';
 import HooksCakeContainer from './components/HooksCakeContainer';
 import IceCreamContontainer from './components/IceCreamContontainer';
 import NewCakeContainer from './components/NewCakeContainer';
+import UserContainer from './components/UserContainer';
 import store from './redux/store'
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-      <HooksCakeContainer />
-        <CakeContainer />
+      {/* <HooksCakeContainer /> */}
+        {/* <CakeContainer />
         <IceCreamContontainer />
-        <NewCakeContainer />
+        <NewCakeContainer /> */}
+
+        <UserContainer />
       </div>
     </Provider>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,11 @@
 import { createStore, applyMiddleware } from 'redux'
 // import cakeReducer from './cake/cakeReducer'
+import thunk from 'redux-thunk'
 import logger from 'redux-logger'
 import { composeWithDevTools } from 'redux-devtools-extension';
 import rootReducer from './rootReducer'
 
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)))
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger, thunk)))
 
 export default store
